b3ff222a46e3c36aab8c24f06845716c53311a52
AGR Group @ IISc Chemical Engineering <70911775+agrgroup@users.noreply.github.com>
2022-10-09 00:35:25 +0530

    Update xyz_num.m

--- Nanopore_Generation/Supplementary_Codes/xyz_num.m
+++ Nanopore_Generation/Supplementary_Codes/xyz_num.m
@@ -3,17 +3,17 @@ function [xyz_in] = xyz_num(polys,n,num,type)
 %input type 1 for shape factor, 2 for minor axis and 3 for major axis
 str=string(n);
 if type==1
-filename3="shape_factor"+str+".xlsx";
+  filename3="shape_factor"+str+".xlsx";
 elseif type==2 
- filename3="minor_axis"+str+".xlsx";   
+  filename3="minor_axis"+str+".xlsx";   
 else
-    filename3="major_axis"+str+".xlsx";
+  filename3="major_axis"+str+".xlsx";
 end
 data=xlsread(filename3);
 polys_index=data(:,1);
 
 if num>1
-num=polys_index(end);
+  num=polys_index(end);
 else
   num=polys_index(1);
 end
